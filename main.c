b1e6e0cbdd30b84c415e624ab2e5a7417ca4bfcb
joseph468 <josephalba2006@outlook.com>
2023-07-05 22:11:15 +0100

    Epic colours

--- main.c
+++ main.c
@@ -30,20 +30,26 @@ int main(int argc, char *argv[]) {
 	// Create window
 	int y, x;
 	getmaxyx(stdscr, y, x);
-	WINDOW *win = newwin(y, x, 0, 0); // Y, X, Y, X
-	refresh();
+	WINDOW *win = newwin(y-1, x, 0, 0); // Y, X, Y, X
 	box(win, 0, 0);	
+	keypad(win, true);
+	init_pair(1, COLOR_CYAN, COLOR_BLACK);
+	wattron(win, COLOR_PAIR(1));
 	wrefresh(win);
 	// Startup variables
 	int ch = '\0'; 
 	Line fileHead = {malloc(8), NULL, NULL};
 	Line *currentLine;
 	currentLine = &fileHead;	
+	// Bottom Bar setup
+	WINDOW *bar = newwin(1, x, y-1, 0);
+	init_color(24, 289, 211, 735);
+	init_pair(2, 24, 24);
+	wattron(bar, COLOR_PAIR(2));
+	wbkgd(bar, COLOR_PAIR(2));
+	wrefresh(bar);
 
-	// Middle stuff
-	keypad(win, true);
-	init_pair(1, COLOR_GREEN, COLOR_BLACK);
-	wattron(win, COLOR_PAIR(1));
+	// Main loop
 	while (1) {
 		ch = wgetch(win);
 		getyx(win, y, x);
